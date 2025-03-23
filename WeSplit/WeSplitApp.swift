a854f7789d89b859f276956fb4cb1d359d7c80c9
Gabriel Jesus Santos <84652898+bieeel9@users.noreply.github.com>
2025-03-23 17:12:13 -0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Gabriel Santos on 23/03/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
